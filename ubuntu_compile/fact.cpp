89a513e3e51116545154df3c0ba24fb6f550e50c
JackZhang9 <52847412+JackZhang9@users.noreply.github.com>
2023-04-26 14:37:15 +0800

    Add files via upload

---
+++ ubuntu_compile/fact.cpp
@@ -0,0 +1,8 @@
+#include"chapter6.h"
+#include<iostream>
+
+int  fact(int val)
+{
+    std::cout<<val<<"*"<<val<<"="<<val*val;
+    return 0;
+}
---
+++ ubuntu_compile/factMain.cpp
@@ -0,0 +1,8 @@
+#include"chapter6.h"
+#include<iostream>
+
+int main()
+{
+    fact(2);
+    return 0;
+}
